04ca5bd8fdeb32d6544b4643f58a0a4e2607032c
Adithya D <rayadithya@gmail.com>
2023-09-20 23:04:13 +0530

    Update JS

--- script.js
+++ script.js
@@ -1,17 +1,102 @@
-const grid = ( () => { 
 
-    const create = () => 
+
+
+const player = (name,symbol) => 
+    {  
+        
+        const winner = () => { console.log(`${symbol} wins!`)}
+        return { winner, name, symbol }
+        
+
+    }
+
+
+const gameboard = ( () => 
+    {   
+        const grids = [];
+
+
+        const create = () => 
         { 
-        var grids = [];
+        
         for (var i = 0;i<9;i++)
             {
                 grids[i] = document.createElement('div')
+                grids[i].textContent = "h"
                 grids[i].classList.add('grids')
                 document.querySelector('.biggrid').append(grids[i]);
+
+                grids[i].addEventListener('click',()=> {
+                    console.log("Clicked index:", i)
+                    gameboard.makeMove(i, currentPlayer.symbol)
+                })
+
             }
-        }
-return {create};
-                    })();
+        };
+
+        const victory = (symbol) =>
+            {
+                if (grids[1].textContent == symbol && grids[5].textContent == symbol && grids[9].textContent == symbol)
+                    {
+                        return true;
+
+                    }
+
+                else if (grids[3].textContent == symbol && grids[5].textContent == symbol && grids[7].textContent == symbol)
+                    {
+                        return true;
+                    }
+
+                else if (grids[1].textContent == symbol && grids[2].textContent == symbol && grids[3].textContent == symbol)
+                    {
+                        return true;
+                    }
+
+                else if (grids[4].textContent == symbol && grids[5].textContent == symbol && grids[6].textContent == symbol)
+                    {
+                        return true;
+                    }
+
+                else if (grids[7].textContent == symbol && grids[8].textContent == symbol && grids[9].textContent == symbol)
+                    {
+                        return true;
+                    }
+                
+                else 
+                    {
+                        return false;
+                    }
+            };
+
+            
+            const makeMove = (index,symbol) =>
+            {
+                if (grids[index].textContent == "h")
+                    {
+                        grids[index].textContent = "hi"
+                        return true;
+                        
+                    }
+                return false;
 
 
-grid.create()
+            };
+
+
+            return { makeMove, victory, create }
+
+
+    }
+
+)();
+
+const play = () =>
+{
+
+}
+
+
+gameboard.create()
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
+let currentPlayer = player1

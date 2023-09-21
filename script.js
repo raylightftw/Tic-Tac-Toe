e24b07147bb7fe85b759ea95c5aa60f69f11e0e1
Adithya D <rayadithya@gmail.com>
2023-09-21 18:44:24 +0530

    Update JS

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const player = (name,symbol) =>
 
 const gameboard = ( () => 
     {   
-        const grids = [];
+        let grids = [];
 
 
         const create = () => 
@@ -22,42 +22,57 @@ const gameboard = ( () =>
         for (var i = 0;i<9;i++)
             {
                 grids[i] = document.createElement('div')
-                grids[i].textContent = "h"
+                grids[i].textContent = ""
                 grids[i].classList.add('grids')
                 document.querySelector('.biggrid').append(grids[i]);
 
-                grids[i].addEventListener('click',()=> {
-                    console.log("Clicked index:", i)
-                    gameboard.makeMove(i, currentPlayer.symbol)
-                })
+                (function (index) {
+                    grids[index].addEventListener('click', () => {
+                        if(gameboard.makeMove(index, currentPlayer.symbol))
+                        {gameboard.playerSwap(currentPlayer,player1,player2);
+
+                            if (gameboard.victory(currentPlayer.symbol)) 
+                            {
+                                console.log(`${currentPlayer.name} wins`);
+                                gameboard.clearBoard();
+                            }
+                    }});
+                })(i);
 
             }
         };
 
+        const clearBoard = () => {
+            const bigGrid = document.querySelector('.biggrid');
+            bigGrid.innerHTML = "";
+            grids = [];
+            create(); // Recreate the board and event listeners
+        };
+
         const victory = (symbol) =>
             {
-                if (grids[1].textContent == symbol && grids[5].textContent == symbol && grids[9].textContent == symbol)
+                if (grids[0].textContent == symbol && grids[4].textContent == symbol && grids[8].textContent == symbol)
                     {
                         return true;
 
                     }
 
-                else if (grids[3].textContent == symbol && grids[5].textContent == symbol && grids[7].textContent == symbol)
+                else if (grids[2].textContent == symbol && grids[4].textContent == symbol && grids[6].textContent == symbol)
                     {
                         return true;
                     }
 
-                else if (grids[1].textContent == symbol && grids[2].textContent == symbol && grids[3].textContent == symbol)
+                else if (grids[0].textContent == symbol && grids[1].textContent == symbol && grids[2].textContent == symbol)
                     {
                         return true;
                     }
 
-                else if (grids[4].textContent == symbol && grids[5].textContent == symbol && grids[6].textContent == symbol)
+                else if (grids[3].textContent == symbol && grids[4].textContent == symbol && grids[5].textContent == symbol)
                     {
                         return true;
                     }
 
-                else if (grids[7].textContent == symbol && grids[8].textContent == symbol && grids[9].textContent == symbol)
+                else if (grids[6].textContent == symbol && grids[7].textContent == symbol && grids[8].textContent == symbol)
                     {
                         return true;
                     }
@@ -66,14 +81,18 @@ const gameboard = ( () =>
                     {
                         return false;
                     }
+
+
+
+                
             };
 
             
             const makeMove = (index,symbol) =>
             {
-                if (grids[index].textContent == "h")
+                if (grids[index].textContent == "")
                     {
-                        grids[index].textContent = "hi"
+                        grids[index].textContent = symbol
                         return true;
                         
                     }
@@ -82,8 +101,23 @@ const gameboard = ( () =>
 
             };
 
+            const playerSwap = (current,p1,p2) => 
+            {
+                
+                if(currentPlayer.symbol == p1.symbol)
+                    {
+                        currentPlayer = p2
+                    }
 
-            return { makeMove, victory, create }
+                else if(currentPlayer.symbol == p2.symbol)
+                    {
+                        currentPlayer = p1
+                    }
+                
+                
+            }
+
+            return { makeMove, victory, create, playerSwap,clearBoard }
 
 
     }
